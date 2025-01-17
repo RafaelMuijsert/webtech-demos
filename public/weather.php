1e49aa347a5eb023c40bc4d5dfc0b9102cc48d62
toonvangelderen <t.vangelderen@uva.nl>
2025-01-17 21:22:57 +0100

    fixed weather js location

--- public/weather.php
+++ public/weather.php
@@ -17,8 +17,6 @@ require_once("../src/database.php");
   <body>
     <?php require_once "../src/components/header.php" ?>
     <?php require_once "../src/components/navbar.php" ?>
-    <script src="/js/main.js"></script>
-    <script src="/js/pages/weather.js"></script>
     <main>
       <div class="container">
         <h1>Weather Information</h1>
@@ -30,5 +28,7 @@ require_once("../src/database.php");
       </div>
     </main>
     <?php require_once "../src/components/footer.php" ?>
+    <script src="js/main.js"></script>
+    <script src="js/pages/weather.js"></script>
   </body>
 </html>
