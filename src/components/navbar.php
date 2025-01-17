6bdc1e41d13e3c2b550c8cf4b314b8a2f2d66dcb
toonvangelderen <t.vangelderen@uva.nl>
2025-01-17 20:54:44 +0100

    fixed Home

--- src/components/navbar.php
+++ src/components/navbar.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="css/partial/navbar.css">
 <nav>
     <ul>
-        <li><a href="">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="style.php">Style</a></li>
         <li><a href="session.php">Session</a></li>
         <li><a href="register.php">Register</a></li>
