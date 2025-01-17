9462080d81bca2c5b7aeefaa984ccc0bdef75bd2
toonvangelderen <t.vangelderen@uva.nl>
2025-01-17 20:47:30 +0100

    fixed rel paths

--- public/session.php
+++ public/session.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Webtech Demo</title>
-    <link rel="stylesheet" href="/css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
   </head>
   <body>
